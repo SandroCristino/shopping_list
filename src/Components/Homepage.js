988d22ab24ad5a7cf32ebad87ed39d93cd2e9aa9
SandyWezzy <72725989+SandyWezzy@users.noreply.github.com>
2023-05-10 15:16:48 +0700

    builded homepage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import '../Styles/Homepage.css'
 import Navbar from './Navbar'
+import { Link } from 'react-router-dom'
+import Footer from './Footer'
 
 export default function Homepage() {
   return (
-    <div>
+    <div className='outerDiv'>
       <Navbar />
+      <div className="mainContent align-items-center justify-content-center d-flex">
+        <h1 className='shadow'>Welcome To Paradise</h1>
+        <Link className='shopButton btn btn-success mb-4' to='/shopping-list'>Start Browsing</Link>
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import RouteSwitch from './Components/RouteSwitch';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
